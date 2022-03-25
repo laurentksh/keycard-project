ac021c7b9d2c6c0970e9f40b0956741fb3d504bd
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2022-03-25 14:18:24 +0100

    dump

--- KeyCardIoTApp/KeyCardIoTApp/Program.cs
+++ KeyCardIoTApp/KeyCardIoTApp/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace KeyCardIoTApp;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace KeyCardIoTApp;
 
 public static class Program
 {
     public static int Main(string[] args)
     {
-
+        HubConnectionBuilder builder = new HubConnectionBuilder();
+        builder.WithAutomaticReconnect();
+        builder.WithUrl("");
         return 0;
     }
 }
