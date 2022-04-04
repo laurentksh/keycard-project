13d6623bf0588ed62a82c9cd60ed417dfc3d347a
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2022-04-04 13:22:31 +0200

    Implemented IoT app

--- KeyCardIoTApp/KeyCardIoTApp/Program.cs
+++ KeyCardIoTApp/KeyCardIoTApp/Program.cs
@@ -1,14 +1,48 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using KeyCardIoTApp.Models;
+using KeyCardIoTApp.Services;
+using Refit;
 
 namespace KeyCardIoTApp;
 
 public static class Program
 {
-    public static int Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        HubConnectionBuilder builder = new HubConnectionBuilder();
-        builder.WithAutomaticReconnect();
-        builder.WithUrl("");
-        return 0;
+        INfcReader nfcReader;
+        IPunchService punchService = RestService.For<IPunchService>(
+            Environment.GetEnvironmentVariable("WebServiceUri") ??
+            args.GetValue(1) as string ??
+            "http://localhost:9000");
+
+        if ((args.GetValue(0) as string)?.ToLower() == "prd")
+            nfcReader = new TestNfcReader();
+        else
+            nfcReader = new PhysicalPn532NfcReader();
+
+        while (true)
+        {
+            string token;
+            try
+            {
+                token = await nfcReader.ReadAsync(CancellationToken.None);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Could not read NFC tag value: " + ex.ToString());
+                continue;
+            }
+
+            PunchViewModel vm;
+            try
+            {
+                vm = await punchService.RegisterPunch(token);
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine("Could not reach server: " + ex.ToString());
+                continue;
+            }
+
+            Console.WriteLine($"Successfully registered punch for user {vm.UserId} ({vm.Id})");
+        }
     }
 }
