d3f2c09825eaac5d53cdae7c0fcbcc0375bd86f8
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2022-04-04 16:11:14 +0200

    Fix build error

--- KeyCardWebServices/KeyCardWebServices/Services/PunchService.cs
+++ KeyCardWebServices/KeyCardWebServices/Services/PunchService.cs
@@ -1,6 +1,33 @@
-﻿namespace KeyCardWebServices.Services;
+﻿using KeyCardWebServices.Data.Models;
+using KeyCardWebServices.Models.Dtos;
+using KeyCardWebServices.Models.ViewModels;
+
+namespace KeyCardWebServices.Services;
 
 public class PunchService : IPunchService
 {
+    public Task<PunchViewModel> RegisterPunch(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<PunchViewModel> EditPunch(AppUser user, PunchEditDto editDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PunchViewModel> DeletePunch(AppUser user, Guid punchId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PunchViewModel> GetPunch(AppUser user, Guid punchId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<PunchViewModel>> GetPunches(AppUser user, PunchFilterDto filter)
+    {
+        throw new NotImplementedException();
+    }
 }
